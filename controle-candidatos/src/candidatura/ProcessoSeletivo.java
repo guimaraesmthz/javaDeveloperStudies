42ad808da04f253979adb3e41adaf73ad18112ef
Matheus Guimarães <br.matheusguimaraes@gmail.com>
2023-08-29 23:01:12 -0300

    Pequeno código testando métodos
    Pequeno código testando métodos

---
+++ controle-candidatos/src/candidatura/ProcessoSeletivo.java
@@ -0,0 +1,23 @@
+package candidatura;
+
+public class ProcessoSeletivo {
+
+	public static void main(String[] args) {
+		analisarCandidato(1900.0);
+		analisarCandidato(2220.0);
+		analisarCandidato(2000.0);
+
+	}
+	
+	static void analisarCandidato(double salarioPretendido) {
+		double salarioBase = 2000.0;
+		if(salarioBase > salarioPretendido) {
+			System.out.println("LIGAR PARA O CANDIDATO.");
+		}else if(salarioBase == salarioPretendido) {
+			System.out.println("LIGAR PARA O CANDIDATO COM CONTRA PROPOSTA.");
+		}else {
+			System.out.println("AGUARDANDO OS DEMAIS CANDIDATOS.");
+		}
+	}
+
+}
